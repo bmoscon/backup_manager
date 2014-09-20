3e8aae4dcfd496be8da1ff4b994113ceb112cfc7
Bryant Moscon <bmoscon@gmail.com>
2014-09-20 12:00:50 -0400

    fix indentations

--- src/config_parse.hpp
+++ src/config_parse.hpp
@@ -58,37 +58,37 @@ class ConfigParse {
 typedef std::unordered_map<std::string, std::string> section;
 
 public:
-  ConfigParse(const char *file);
-  ConfigParse(const std::string &file);
-  
+    ConfigParse(const char *file);
+    ConfigParse(const std::string &file);
+    
 
-  typedef section::iterator iterator;
-  typedef section::const_iterator const_iterator;
-  
-  iterator begin(const std::string &section);
-  iterator end(const std::string &section);
-  std::string getValue(const std::string &section, const std::string &name);
-  std::vector<std::string> getSections() const;
-
+    typedef section::iterator iterator;
+    typedef section::const_iterator const_iterator;
+    
+    iterator begin(const std::string &section);
+    iterator end(const std::string &section);
+    std::string getValue(const std::string &section, const std::string &name);
+    std::vector<std::string> getSections() const;
+    
 private:
-  void parse(const char *file);
-  
-  std::unordered_map<std::string, section> config;
+    void parse(const char *file);
+    
+    std::unordered_map<std::string, section> config;
 };
 
 
 class ConfigParseEx : public std::exception {
 public:
   
-  ConfigParseEx(const char* m);
-  ConfigParseEx(const std::string& m);
-  ConfigParseEx(const char* m, const unsigned int l);
-  ConfigParseEx(const std::string& m, const unsigned int l);
-
-  
-  virtual const char* what() const throw();
-  
-  std::string msg;
+    ConfigParseEx(const char* m);
+    ConfigParseEx(const std::string& m);
+    ConfigParseEx(const char* m, const unsigned int l);
+    ConfigParseEx(const std::string& m, const unsigned int l);
+    
+    
+    virtual const char* what() const throw();
+    
+    std::string msg;
 };
 
 #endif
